ddf5a20a8ecb0377e420244b02c23b5faeb0b20f
Avee <avi.pansara@gmail.com>
2021-09-25 15:27:14 -0500

    functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { AlertService } from './services/alert.service';
-import { Alert } from './models/model.alert';
+import {Component} from '@angular/core';
+import {AlertService} from './services/alert.service';
+import {Alert} from './models/model.alert';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
   public allAlerts: Alert[] = [];
+  public originalAllAlerts: Alert[] = [];
+  public activeFilters: any = {};
 
   constructor(
     private alertService: AlertService,
@@ -20,6 +25,54 @@ export class AppComponent {
 
   async getAlerts() {
     this.allAlerts = await this.alertService.getAlerts().toPromise();
-    console.log(this.allAlerts)
+    this.originalAllAlerts = _.cloneDeep(this.allAlerts);
   }
+
+  filterAlertsBy(filter: any) {
+    const {
+      category,
+      value,
+    } = filter;
+
+    if (!this.activeFilters[category]) {
+      this.activeFilters[category] = value;
+    }
+    this.allAlerts = this.activeFilterData(this.activeFilters, this.originalAllAlerts);
+  }
+
+  removeFilter(filter: any) {
+    delete this.activeFilters[filter];
+    this.allAlerts = this.activeFilterData(this.activeFilters, this.originalAllAlerts);
+  }
+
+  activeFilterData(activeFilters: any, data: Alert[]): Alert[] {
+    let filteredData: Alert[]  = _.cloneDeep(data);
+    if (activeFilters['Severity']) {
+      const severityFilter = activeFilters['Severity'];
+      filteredData = data.filter(a => severityFilter === a.Severity)
+    }
+    if (activeFilters['Protocol']) {
+      const protocolFilter = activeFilters['Protocol'];
+      filteredData = filteredData.filter(a => protocolFilter === a.Protocol)
+    }
+    if (activeFilters['ClientIP']) {
+      const clientIPFilter = activeFilters['ClientIP'];
+      filteredData = filteredData.filter(a => clientIPFilter === a.ClientIP)
+    }
+    if (activeFilters['ClientCountry']) {
+      const countryFilter = activeFilters['ClientCountry'];
+      filteredData = filteredData.filter(a => countryFilter === a.ClientCountry)
+    }
+    return filteredData;
+  }
+
+  reset() {
+    this.activeFilters = {};
+    this.allAlerts = _.cloneDeep(this.originalAllAlerts);
+  }
+
+  isFilterActive(): boolean {
+    return _.isEmpty(this.activeFilters);
+  }
+
 }
--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,18 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, Output, EventEmitter, SimpleChanges} from '@angular/core';
+import { Alert } from 'src/app/models/model.alert';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent {
 
   @Input() category = '';
   @Input() title = '';
+  @Input() data: Alert[] = [];
+  @Output() selectCategory = new EventEmitter<{}>();
 
-  constructor() { }
+  categoryData: any = {};
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges) {
+    this.categoryData = {};
+    this.data.forEach(a => {
+      const newAlert = Object.create(a);
+      const displayBy = this.category && newAlert[this.category];
+      if (displayBy && this.categoryData[displayBy]) {
+        this.categoryData[displayBy] = this.categoryData[displayBy] + 1;
+      } else if (displayBy) {
+        this.categoryData[displayBy] = 1;
+      }
+    })
+  }
+
+  filterBy(value: any) {
+    this.selectCategory.emit({
+      category: this.category,
+      value,
+    })
   }
 
 }
