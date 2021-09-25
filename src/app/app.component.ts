cd5c15f78f4cf4f90c258ffe3c8d61d757be4981
Avee <avi.pansara@gmail.com>
2021-09-25 15:04:15 -0500

    separate out the small components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertService } from './services/alert.service';
+import { Alert } from './models/model.alert';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alerts';
+  public allAlerts: Alert[] = [];
+
+  constructor(
+    private alertService: AlertService,
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.getAlerts();
+  }
+
+  async getAlerts() {
+    this.allAlerts = await this.alertService.getAlerts().toPromise();
+    console.log(this.allAlerts)
+  }
 }
